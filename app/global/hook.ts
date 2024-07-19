618236516dce5328579c32d1b54f59663434afee
TM-Code-009 <osakweterrynduka@gmail.com>
2024-07-19 13:24:28 +0100

    adjusted

--- app/global/hook.ts
+++ app/global/hook.ts
@@ -1,7 +1,17 @@
-import { useDispatch, useSelector, useStore } from "react-redux";
+// import { useDispatch, useSelector, useStore } from "react-redux";
+// import type { RootState, AppDispatch, AppStore } from "./store";
+
+// // Use throughout your app instead of plain `useDispatch` and `useSelector`
+// export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+// export const useAppSelector = useSelector.withTypes<RootState>();
+// export const useAppStore = useStore.withTypes<AppStore>();
+
+
+import { TypedUseSelectorHook, useDispatch as useDispatchBase, useSelector as useSelectorBase, useStore as useStoreBase } from "react-redux";
 import type { RootState, AppDispatch, AppStore } from "./store";
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
-export const useAppSelector = useSelector.withTypes<RootState>();
-export const useAppStore = useStore.withTypes<AppStore>();
+export const useAppDispatch = () => useDispatchBase<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelectorBase;
+export const useAppStore = () => useStoreBase<AppStore>();
+
